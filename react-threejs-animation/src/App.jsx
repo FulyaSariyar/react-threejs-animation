4b88ec91055cd1fcf5a9e94318e83c03c347c340
Fulya Sariyar <fulyasariyar@outlook.com>
2022-08-26 17:08:05 +0300

    two (mesh)

--- react-threejs-animation/src/App.jsx
+++ react-threejs-animation/src/App.jsx
@@ -1,12 +1,30 @@
-import React, { Component } from 'react'
+import React, { useRef } from 'react'
+import {Canvas, useFrame} from '@react-three/fiber'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    )
-  }
-}
+//scene => sahne
+//geometry => şekil
+//metarial => renk
+//mesh => geometry, metarial
+
+const Box = (props) =>{
+  const ref = useRef();
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+
+  return(
+  <mesh {...props} ref = {ref} position={[0,0,0]}>
+    <boxBufferGeometry args={[1,1,1]}> </boxBufferGeometry>
+    <meshBasicMaterial color ={'#fff'} ></meshBasicMaterial>
+  </mesh>
+  );
+};
+
+const App = () => {
+  return (
+    <Canvas>
+    <ambientLight></ambientLight>
+    <pointLight position={[10,10,10]}></pointLight>
+    <Box position={[-1.2,0,0]}></Box>
+    </Canvas>
+  );
+};
 export default App
