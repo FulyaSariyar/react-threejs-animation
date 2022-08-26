3527f9a4e15a7f81295de14bedcf4cf233536fd2
Fulya Sariyar <fulyasariyar@outlook.com>
2022-08-26 19:29:24 +0300

    three - kiki ekrana getirildi.

--- react-threejs-animation/src/App.jsx
+++ react-threejs-animation/src/App.jsx
@@ -1,29 +1,15 @@
-import React, { useRef } from 'react'
+import React, { Suspense, useRef } from 'react'
 import {Canvas, useFrame} from '@react-three/fiber'
-
-//scene => sahne
-//geometry => şekil
-//metarial => renk
-//mesh => geometry, metarial
-
-const Box = (props) =>{
-  const ref = useRef();
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
-
-  return(
-  <mesh {...props} ref = {ref} position={[0,0,0]}>
-    <boxBufferGeometry args={[1,1,1]}> </boxBufferGeometry>
-    <meshBasicMaterial color ={'#fff'} ></meshBasicMaterial>
-  </mesh>
-  );
-};
+import Cat from './components/Cat'
 
 const App = () => {
   return (
     <Canvas>
     <ambientLight></ambientLight>
     <pointLight position={[10,10,10]}></pointLight>
-    <Box position={[-1.2,0,0]}></Box>
+    <Suspense fallback ={null}>
+    <Cat></Cat>
+    </Suspense>
     </Canvas>
   );
 };
